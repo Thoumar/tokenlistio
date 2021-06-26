149a61c3dac0230c5122f1507b1f0749c7390df7
Thomas Oumar <thomas.oumar@abtasty.com>
2021-06-26 17:42:20 +0200

    [NEW] Adding fetch request to nomics API

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-	constructor(props) {
+	constructor() {
 		super();
 		this.state = {
-			tokens: [
-				{
-					name: "Bitcoin",
-				},
-				{
-					name: "Ethereum",
-				},
-				{
-					name: "Litecoin",
-				},
-			],
+			tokens: [],
 		};
+	}
+
+	componentDidMount() {
+		fetch("https://api.nomics.com/v1/currencies?key=e0e5ade77feffe8318fc6dc6c542760bb1e020d1&ids=BTC,ETH,XRP&attributes=id,name,logo_url")
+			.then((response) => response.json())
+			.then((tokens) => this.setState({ tokens }));
 	}
 
 	render() {
 		return (
 			<div className="App">
 				{this.state.tokens.map((token) => (
-					<h1>{token.name}</h1>
+					<div key={token.id}>
+						<img src={token.logo_url} alt={`${token.name} logo`} width="50" />
+						<h1>{token.name}</h1>
+					</div>
 				))}
 			</div>
 		);
