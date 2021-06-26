e131efcf7265dc8285d4092aa1cf71dd6127b425
Thomas Oumar <thomas.oumar@abtasty.com>
2021-06-26 19:02:08 +0200

    Adding a search-box component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { TokenList } from "./components/token-list/token-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 import "./App.css";
 
 class App extends Component {
@@ -7,21 +8,29 @@ class App extends Component {
 		super();
 		this.state = {
 			tokens: [],
+			search: "",
 		};
 	}
 
 	componentDidMount() {
 		fetch(
-			"https://api.nomics.com/v1/currencies?key=e0e5ade77feffe8318fc6dc6c542760bb1e020d1&ids=BTC,ETH,ADA,XRP,USDC,USDT,BNB,CHSB,CHZ,UNI,EOS,ENJ,AVAX,XLM,HBAR,ZEC,KNCL,UTK,OXT,STMX,RSR,COIN,API3,OMI&attributes=id,name,logo_url"
+			"https://api.nomics.com/v1/currencies?key=e0e5ade77feffe8318fc6dc6c542760bb1e020d1&ids=BTC,ETH,ADA,XRP,USDC,USDT,BNB,CHSB,CHZ,UNI,EOS,ENJ,AVAX,XLM,HBAR,ZEC,KNCL,UTK,OXT,STMX,RSR,COIN,API3,OMI,DOGE&attributes=id,name,logo_url"
 		)
 			.then((response) => response.json())
-			.then((tokens) => this.setState({ tokens }));
+			.then((tokens) => {
+				console.log(tokens);
+				this.setState({ tokens });
+			});
 	}
 
 	render() {
+		const { tokens, search } = this.state;
+		const filteredTokens = tokens.filter((token) => token.name.toLowerCase().includes(search.toLowerCase()));
+
 		return (
 			<div className="App">
-				<TokenList tokens={this.state.tokens} />
+				<SearchBox placeholder="Search for a token" handleChange={(e) => this.setState({ search: e.target.value })} />
+				<TokenList tokens={filteredTokens} />
 			</div>
 		);
 	}
